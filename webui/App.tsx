f9c6a05ca51b5709a3704dfaa37d9bfcb79c3172
Brandon Pelton-Cox <>
2024-04-26 12:50:19 -0500

    move styles inline

--- webui/App.tsx
+++ webui/App.tsx
@@ -1,6 +1,6 @@
 import { useDevToolsPluginClient, type EventSubscription } from "expo/devtools";
 import { useEffect } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function App() {
   const client = useDevToolsPluginClient("network-plugin");
@@ -23,46 +23,71 @@ export default function App() {
   }, [client]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 16,
+          marginBottom: 16,
+        }}
+      >
         That's the Web UI of the DevTools plugin. You can now edit the UI in the
         App.tsx.
       </Text>
-      <Text style={[styles.text, styles.devHint]}>
+      <Text
+        style={[
+          {
+            fontSize: 16,
+            marginBottom: 16,
+          },
+          {
+            color: "#666",
+          },
+        ]}
+      >
         For development, you can also add `devServer` query string to specify
         the WebSocket target to the app's dev server.
       </Text>
-      <Text style={[styles.text, styles.devHint]}>For example:</Text>
+      <Text
+        style={[
+          {
+            fontSize: 16,
+            marginBottom: 16,
+          },
+          {
+            color: "#666",
+          },
+        ]}
+      >
+        For example:
+      </Text>
       <Pressable
         onPress={() => {
           window.location.href =
             window.location.href + "?devServer=localhost:8080";
         }}
       >
-        <Text style={[styles.text, styles.textLink]}>
+        <Text
+          style={[
+            {
+              fontSize: 16,
+              marginBottom: 16,
+            },
+            {
+              color: "#007AFF",
+              textDecorationLine: "underline",
+            },
+          ]}
+        >
           {`${window.location.href}?devServer=localhost:8080`}
         </Text>
       </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 16,
-    marginBottom: 16,
-  },
-  devHint: {
-    color: "#666",
-  },
-  textLink: {
-    color: "#007AFF",
-    textDecorationLine: "underline",
-  },
-});
